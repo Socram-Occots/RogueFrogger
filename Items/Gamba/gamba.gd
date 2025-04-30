6d37aacb69adef6eb47ab4883f8dfa7457bc035f
Marcos Stocco <soramoccots@gmail.com>
2025-04-29 19:33:32 -0500

    more work on gamba

--- Items/Gamba/gamba.gd
+++ Items/Gamba/gamba.gd
@@ -4,13 +4,17 @@ extends Node
 @export var gamba_resut_time_seconds : int = 5
 @onready var cycleitemcounter : int = 0
 @onready var itemcycletimer : Timer = $HBoxContainer/ItemCycleTimer
-@onready var numofcycles : int = len(item_pool) - 1
-@onready var timepercycle : float =  gamba_resut_time_seconds / float(numofcycles)
-@onready var timepoeritemcycle : float = timepercycle / numofcycles
+@onready var timepercycle : float  
+@onready var timeperitemcycle : float
+@onready var len_item_pool : int = len(item_pool)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	itemcycletimer.start(timepoeritemcycle)
+	if len_item_pool < 1:
+		return
+	timepercycle = gamba_resut_time_seconds / float(len_item_pool - 1)
+	timeperitemcycle = timepercycle / len_item_pool - 1
+	start_cycle()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,25 +22,26 @@ func _process(delta: float) -> void:
 
 
 func _on_item_cycle_timer_timeout() -> void:
-	var len_item_pool : int = len(item_pool)
+	cycleitemcounter += 1
+	
+	if cycleitemcounter == len_item_pool - 1:
+		cycleitemcounter = 0
+		item_pool.remove_at(randi_range(0, len_item_pool - 1))
+		len_item_pool -= 1
+		item_pool.shuffle()
+		timeperitemcycle = timepercycle / len_item_pool - 1
+	
+	start_cycle()
+
+
+func start_cycle() -> void:
+	print(cycleitemcounter)
 	
 	if len_item_pool < 2:
 		present_winner()
 		return
-	
-	cycleitemcounter += 1
-	if cycleitemcounter == numofcycles:
-		numofcycles -= 1
-		cycleitemcounter = 0
-		
-		item_pool.remove_at(randi_range(0, len_item_pool - 1))
-		if len_item_pool < 2:
-			present_winner()
-			return
-		
-		item_pool.shuffle()
-		timepoeritemcycle = timepercycle / len(item_pool)
-		itemcycletimer.start(timepoeritemcycle)
+
+	itemcycletimer.start(timeperitemcycle)
 
 func present_winner() -> void:
 	pass
