2b72ea6e37ce1a86a09c1e58f883542cb4551c5a
Marcos Stocco <soramoccots@gmail.com>
2025-04-30 19:35:18 -0500

    gamba selecton now selects a winner in the appropriate about of time

--- Items/Gamba/gamba.gd
+++ Items/Gamba/gamba.gd
@@ -8,12 +8,14 @@ extends Node
 @onready var timeperitemcycle : float
 @onready var len_item_pool : int = len(item_pool)
 
+@onready var total_sec : float = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if len_item_pool < 1:
 		return
 	timepercycle = gamba_resut_time_seconds / float(len_item_pool - 1)
-	timeperitemcycle = timepercycle / len_item_pool - 1
+	timeperitemcycle = timepercycle / (len_item_pool)
 	start_cycle()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,20 +25,22 @@ func _process(delta: float) -> void:
 
 func _on_item_cycle_timer_timeout() -> void:
 	cycleitemcounter += 1
+	total_sec += timeperitemcycle
 	
-	if cycleitemcounter == len_item_pool - 1:
+	if cycleitemcounter == len_item_pool:
 		cycleitemcounter = 0
 		item_pool.remove_at(randi_range(0, len_item_pool - 1))
 		len_item_pool -= 1
 		item_pool.shuffle()
-		timeperitemcycle = timepercycle / len_item_pool - 1
-	
+		if len_item_pool > 1:
+			timeperitemcycle = timepercycle / (len_item_pool)
 	start_cycle()
 
 
 func start_cycle() -> void:
-	print(cycleitemcounter)
-	
+	print("counter: ",cycleitemcounter,"timeperitemcycle: ",timeperitemcycle,"timepercycle: ",timepercycle)
+	print(item_pool)
+	print("item_count: ", len_item_pool)
 	if len_item_pool < 2:
 		present_winner()
 		return
@@ -44,4 +48,5 @@ func start_cycle() -> void:
 	itemcycletimer.start(timeperitemcycle)
 
 func present_winner() -> void:
-	pass
+	print("winner!:", item_pool[cycleitemcounter])
+	print("sec: ", total_sec)
